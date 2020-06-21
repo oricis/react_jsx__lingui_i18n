a1191e115dd3ce469ab48f4d5de0d9574bb87077
moises.alcocer <contacto@ironwoods.es>
2020-06-21 12:29:22 +0200

    added the lingui provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nProvider i18n={i18n}>
+      <App />
+    </I18nProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
